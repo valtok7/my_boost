aefc9b76f721a5cebedcfde465cef2c0a13e377b
valtok7 <morisoba2013@gmail.com>
2022-03-17 06:09:41 +0900

    boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,21 @@
 #include <iostream>
+#include <string_view>
 
-int main(int, char**) {
-    std::cout << "Hello, world!\n";
+#include <boost/asio.hpp>
+
+int main(int argc, char **argv)
+{
+    namespace asio = boost::asio;
+    using asio::ip::tcp;
+
+    asio::io_service io_service;
+    tcp::socket socket(io_service);
+
+    boost::system::error_code error;
+    socket.connect(tcp::endpoint(asio::ip::address::from_string("127.0.0.1"), 31400), error);
+
+    if (error)
+        std::cout << "connect failed : " << error.message() << std::endl;
+    else
+        std::cout << "connected" << std::endl;
 }
